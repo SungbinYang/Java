6fefe8bd404d600f8e77f7d7fbe643c724b3ec9c
SungbinYang <ysb05222@naver.com>
2024-09-20 05:23:45 +0900

    주석(Comment)

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // hello java1을 출력합니다.
+//        System.out.println("Hello Java2");
+
+        /* 여러줄 주석
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}
