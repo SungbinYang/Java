9c1ede6c041f15c9a6df77d97b3034908e4bb9fd
SungbinYang <ysb05222@naver.com>
2024-10-29 06:22:59 +0900

    미션1 초급레벨 문제3번 추가

---
+++ java-start/src/mission/ch02/Resume.java
@@ -0,0 +1,9 @@
+package mission.ch02;
+
+public class Resume {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요, 경기도 시흥에 사는 양성빈입니다.");
+        System.out.println("만나서 반갑습니다.");
+        System.out.println("현재 저는 개발에 푹 빠져 있습니다.");
+    }
+}
