a8df5ffa1c4547de7c73a89dad0bcccff6e5d2f8
SungbinYang <ysb05222@naver.com>
2024-11-01 05:02:18 +0900

    modify

--- java-start/src/loop/While2_1.java
+++ java-start/src/loop/While2_1.java
@@ -5,12 +5,12 @@ public class While2_1 {
         int sum = 0;
 
         sum = sum + 1;
-        System.out.println("i = " + 1 + "sum = " + sum);
+        System.out.println("i = " + 1 + " sum = " + sum);
 
         sum = sum + 2;
-        System.out.println("i = " + 2 + "sum = " + sum);
+        System.out.println("i = " + 2 + " sum = " + sum);
 
         sum = sum + 3;
-        System.out.println("i = " + 3 + "sum = " + sum);
+        System.out.println("i = " + 3 + " sum = " + sum);
     }
 }
