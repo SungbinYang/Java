4de56706b70783830d4b6acb665f5f75ab36f3e3
SungbinYang <ysb05222@naver.com>
2024-11-01 05:51:02 +0900

    modify

--- java-start/src/loop/Break3.java
+++ java-start/src/loop/Break3.java
@@ -4,7 +4,7 @@ public class Break3 {
     public static void main(String[] args) {
         int sum = 0;
 
-        for (int i = 1;;i++) {
+        for (int i = 1; ; i++) {
             sum += i;
 
             if (sum > 10) {
