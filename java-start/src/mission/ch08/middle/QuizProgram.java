5871e6678d9f8ebcae75e0a229405c99da532cb7
SungbinYang <ysb05222@naver.com>
2024-11-02 13:00:46 +0900

    미션7 중급레벨 제출

---
+++ java-start/src/mission/ch08/middle/QuizProgram.java
@@ -0,0 +1,15 @@
+package mission.ch08.middle;
+
+/** 
+* @packageName : mission.ch08.middle 
+* @fileName : QuizProgram 
+* @author : rovert 
+* @date : 2024. 11. 2. 
+* @description : 
+* =========================================================== 
+* DATE 			AUTHOR			 NOTE 
+* ----------------------------------------------------------- 
+* 2024. 11. 2.       rovert         최초 생성 
+*/
+public class QuizProgram {
+}
