dbca9482b314946ba3d3e2c2758c10c99a933664
SungbinYang <ysb05222@naver.com>
2025-02-08 11:31:33 +0900

    문제와 풀이1

---
+++ java-mid2/src/collection/set/test/UniqueNamesTest1.java
@@ -0,0 +1,14 @@
+package collection.set.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNamesTest1 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new HashSet<>(Arrays.asList(inputArr));
+
+        set.iterator().forEachRemaining(System.out::println);
+    }
+}
---
+++ java-mid2/src/collection/set/test/UniqueNamesTest2.java
@@ -0,0 +1,14 @@
+package collection.set.test;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class UniqueNamesTest2 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(inputArr));
+
+        set.iterator().forEachRemaining(System.out::println);
+    }
+}
