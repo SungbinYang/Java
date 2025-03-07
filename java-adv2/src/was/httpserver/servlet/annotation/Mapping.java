8ae9e83fb99306324b110b6f547096f5a78c6df3
SungbinYang <ysb05222@naver.com>
2025-03-07 19:53:08 +0900

    HTTP 서버7 - 애노테이션 서블릿1 - 시작

---
+++ java-adv2/src/was/httpserver/servlet/annotation/Mapping.java
@@ -0,0 +1,10 @@
+package was.httpserver.servlet.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Mapping {
+    String value();
+}
