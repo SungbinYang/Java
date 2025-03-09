1c4675b89adaa1788bd269cb7e134fec8491f5fd
SungbinYang <ysb05222@naver.com>
2025-03-09 14:25:58 +0900

    javaagent 실습

--- java-code-manipulation/classloader-sample/src/main/java/me/sungbin/Masulsa.java
+++ java-code-manipulation/classloader-sample/src/main/java/me/sungbin/Masulsa.java
@@ -1,7 +1,9 @@
 package me.sungbin;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,16 +12,19 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) {
-        try {
-            new ByteBuddy()
-                    .redefine(Moja.class)
-                    .method(named("pullOut"))
-                    .intercept(FixedValue.value("Rabbit!"))
-                    .make()
-                    .saveIn(new File("/Users/rovert/Desktop/study/2024/java/java-code-manipulation/classloader-sample/build/classes/java/main/"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        ClassLoader classLoader = Masulsa.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
+//        try {
+//            new ByteBuddy()
+//                    .redefine(typePool.describe("me.sungbin.Moja").resolve(),
+//                            ClassFileLocator.ForClassLoader.of(classLoader))
+//                    .method(named("pullOut"))
+//                    .intercept(FixedValue.value("Rabbit!"))
+//                    .make()
+//                    .saveIn(new File("/Users/rovert/Desktop/study/2024/java/java-code-manipulation/classloader-sample/build/classes/java/main/"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println(new Moja().pullOut());
     }
